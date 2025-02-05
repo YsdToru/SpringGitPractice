f96c14be035e343b80647e543156c2863b249f00
Yoshida <yoshida-t@kcska.ac.jp>
2025-02-05 17:02:46 +0900

    トップページを表示するコントローラを追加

--- src/main/java/com/example/demo/controller/EmpSampleController.java
+++ src/main/java/com/example/demo/controller/EmpSampleController.java
@@ -1,5 +1,21 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("GitSample")
 public class EmpSampleController {
+
+	@GetMapping("top")
+	public ModelAndView showTopPage(ModelAndView mav) {
+
+		mav.setViewName("top");
+
+		return mav;
+
+	}
 
 }
