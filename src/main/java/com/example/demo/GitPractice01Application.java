41c4820835a3677c3d7856d6da46688e2cf868ee
Yoshida <yoshida-t@kcska.ac.jp>
2025-02-05 16:16:49 +0900

    初回コミット

---
+++ src/main/java/com/example/demo/GitPractice01Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPractice01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitPractice01Application.class, args);
+	}
+
+}
